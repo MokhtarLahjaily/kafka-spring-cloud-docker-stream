71bc6a53ba5ae7121c3aab4bf68b8427bb12c7c6
Mokhtar Lahjaily <mlahjaily25@gmail.com>
2025-12-26 03:49:54 +0100

    Ajout de docker-compose pour l'infrastructure Kafka et Zookeeper

--- src/main/java/ma/lahjaily/KafkaSpringCloudDockerStreamApplication.java
+++ src/main/java/ma/lahjaily/KafkaSpringCloudDockerStreamApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaSpringCloudDockerStreamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaSpringCloudDockerStreamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringCloudDockerStreamApplication.class, args);
+    }
 
 }
